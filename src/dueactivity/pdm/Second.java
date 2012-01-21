181edff7de5ca66fecc1efb9d2e4d409eeefae73
Ruggero Visconti <er_mejo85@hotmail.it>
2012-01-21 11:59:07 +0000

    DueActivity funzionante

---
+++ src/dueactivity/pdm/Second.java
@@ -0,0 +1,17 @@
+package dueactivity.pdm;
+
+import dueactivity.pdm.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity
+{
+
+
+public void onCreate(Bundle savedInstanceState)
+{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+
+}
